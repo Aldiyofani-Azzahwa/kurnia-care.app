9b117a6af208b3736deea7ce49ba045e88c6a5ac
Aldiyofani-Azzahwa <aldiyofaniazzahwa@gmail.com>
2026-06-27 08:37:41 +0700

    fiks

--- resources/views/partials/patient-child-photo.blade.php
+++ resources/views/partials/patient-child-photo.blade.php
@@ -1,16 +1,26 @@
 @php
-    $childPhoto = $patient?->child_photo;
+    $photoValue = $patient?->child_photo;
+    $photoUrl = null;
+
+    if ($photoValue) {
+        if (str_starts_with($photoValue, 'http://') || str_starts_with($photoValue, 'https://')) {
+            $photoUrl = $photoValue;
+        } else {
+            $photoUrl = asset('storage/' . $photoValue);
+        }
+    }
 @endphp
 
 <div class="mb-5">
     <p class="text-sm text-gray-500 mb-2">Foto Anak</p>
 
-    @if ($childPhoto)
-        <a href="{{ asset('storage/' . $childPhoto) }}" target="_blank" class="inline-block">
+    @if ($photoUrl)
+        <a href="{{ $photoUrl }}" target="_blank" class="inline-block">
             <img
-                src="{{ asset('storage/' . $childPhoto) }}"
+                src="{{ $photoUrl }}"
                 alt="Foto Anak {{ $patient->child_name ?? '' }}"
                 class="w-40 h-40 rounded-xl border border-gray-200 object-cover bg-gray-50"
+                onerror="this.onerror=null; this.src='https://placehold.co/300x300?text=Foto+Tidak+Terload';"
             >
         </a>
 
